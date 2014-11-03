0d4343f623853402b792701aec82f833bf928d53
Zhao Wei <pkuzw@126.com>
2014-11-04 00:52:57 +0800

    写文件头部注释

---
+++ Stack.cpp
@@ -0,0 +1,10 @@
+﻿//@file Stack.cpp
+//@brief C语言实现栈
+//@author zhaowei
+//@date 2014.11.03
+//@version 1.0
+
+#include <stdlib.h>	// for malloc()
+#include <string.h>	// for memcpy()
+
+typedef int stack_elem_t; //元素类型
