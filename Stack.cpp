90103f90d799c52bb5a473c1889c86919e5f3583
Zhao Wei <pkuzw@126.com>
2014-11-04 23:58:39 +0800

    写栈的结构体

--- Stack.cpp
+++ Stack.cpp
@@ -8,3 +8,12 @@
 #include <string.h>	// for memcpy()
 
 typedef int stack_elem_t; //元素类型
+
+//@struct
+//@brief 栈的结构体
+
+typedef struct stack_t {
+	int size;	//实际元素个数
+	int capacity; //容量
+	stack_elem_t* elems; //栈的数组
+}stack_t;
