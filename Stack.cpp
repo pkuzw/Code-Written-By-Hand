9af5a833fd9eb643f3a5388865f66dc1f7ef1f4b
Zhao Wei <pkuzw@126.com>
2014-11-05 00:06:45 +0800

    写栈的初始化和释放函数
    stack_init();
    stack_uninit();

--- Stack.cpp
+++ Stack.cpp
@@ -12,8 +12,31 @@ typedef int stack_elem_t; //元素类型
 //@struct
 //@brief 栈的结构体
 
-typedef struct stack_t {
+typedef struct stack_t
+{
 	int size;	//实际元素个数
 	int capacity; //容量
 	stack_elem_t* elems; //栈的数组
 }stack_t;
+
+//@brief 初始化栈
+//@param[inout] s 栈对象的指针
+//@param[in] capacity 初始容量
+//@return 无
+void stack_init(stack_t* s, const int capacity)
+{
+	s->size = 0;
+	s->capacity = capacity;
+	s->elems = (stack_elem_t*)malloc(capacity * sizeof(stack_elem_t));
+}
+
+//@brief 释放栈空间
+//@param[inout] s 栈对象指针
+//@return 无
+void stack_uninit(stack_t* s)
+{
+	s->size = 0;
+	s->capacity = 0;
+	free(s->elems);
+	s->elems = NULL;
+}
