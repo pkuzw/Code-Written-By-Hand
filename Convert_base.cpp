815d5ba47d047de9c2c11da75f424717315dbd89
Zhao Wei <pkuzw@126.com>
2014-11-09 22:03:40 +0800

    进制转换，将d(d <= 16)进制的数转换为10进制
    1. 注意从高位至低位，result = result * d + one.
    其中result初始化为0，one为自高到低的每一位d进制转换为十进制后的整数；
    2. 在保存result时注意使用较大的数据类型，比如long long. 否则容易溢出。譬如用long时在转换deadbeef时就会溢出。

--- Convert_base.cpp
+++ Convert_base.cpp
@@ -61,10 +61,40 @@ void convert_base2(int n, const int d)
 	return;
 }
 
+///@brief 进制转换，将一个d进制整数转换为10进制，d <= 16
+///@param[in] s d进制整数
+///@param[in] d d进制
+///@return 10进制整数
+long long restore(const char s[MAX], const int d)
+{
+	long long result = 0;	//long: -2^31 - 2^31-1，4字节，容易越界
+							//long long: -2^63 - 2^63-1，8字节，不容易越界
+	int one;
 
+	for (int i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			one = s[i] - '0';
+		}
+		else if (s[i] >= 'A' && s[i] <= 'F')
+		{
+			one = s[i] - 'A' + 10;
+		}
+		else
+		{
+			one = s[i] - 'a' + 10;
+		}
+
+		result = result * d + one;	//假设d进制数有n位，从d进制的最高位开始，自高向低每一位都乘以d^(i-1)，其中1 <= i <= n
+	}
+	return result;
+}
 int main()
 {
 	int n = 0, d = 0;
+	
+	cout << "Convert decimal number to d number. d <= 10" << endl;
 	cout << "input d: ";
 	cin >> d;
 	cout << "input n: ";
@@ -74,6 +104,18 @@ int main()
 	cout << endl;
 
 	convert_base2(n, d);
+	cout << endl;
+	cout << "----------------------------------------------" << endl;
+	
+	cout << "Convert  d number to decimal number. d <= 16" << endl;
+	char d_num[MAX] = "";
+	cout << "input string s: ";
+	cin >> d_num;
+
+	cout << "input d: ";
+	cin >> d;
+
+	cout << "restore result: " << restore(d_num, d);
 	cout << endl;
 	return 0;
 }
