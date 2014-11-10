9110c8c812cefa45d08cc87d66ad8a7c6c1047bb
Zhao Wei <pkuzw@126.com>
2014-11-11 01:03:05 +0800

    实现队列部分操作
    1.
    向队尾添加元素：queue_push()。该函数比较复杂，主要要考虑到队列结构体已分配内存是否足够的问题，如果不够，需要分配新的内存，然后将原来内存上的元素转移到新内存空间上，之后还要处理新的front,
    rear和capacity值；
    2. 获取队首元素：queue_front()；
    3. 获取队尾元素：queue_rear()；
    4. 弹出队首元素：queue_pop()。注意要将front的新值取模。
    以上的方法都未测试。

--- Queue.cpp
+++ Queue.cpp
@@ -67,6 +67,66 @@ int queue_size(const queue_t* q)
 	return (q->rear - q->front + q->capacity) % q->capacity;
 }
 
+///@brief 在队尾添加元素
+///@param[in] q 指向队列结构体的指针
+///@param[in] x 要添加的元素
+///@return 无
+void queue_push(queue_t* q, const queue_elem_t x)
+{
+	//如果队列现有内存空间已满
+	if((q->rear + 1) % q->capacity == q->front)
+	{
+		queue_elem_t* tmp = (queue_elem_t*)malloc(q->capacity * 2 * sizeof(queue_elem_t));
+		//如果队首指针在队尾指针的前方
+		if(q->front < q->rear)	
+		{
+			memcpy(tmp, q->elems + q->front, (q->rear - q->front) * sizeof(queue_elem_t));
+		}
+		//如果对手指针在队尾的后方
+		else if(q->front > q->rear)
+		{
+			//将q->elms中q->front至末尾的元素先复制到新的内存空间前半部分
+			memcpy(tmp, q->elems + q->front, (q->capacity - q->front) * sizeof(queue_elem_t));
+
+			//将q->elms中开始至q->rear的元素先复制到新的内存空间后半部分
+			memcpy(tmp + (q->capacity - q->front), q->elems, (q->rear * sizeof(queue_elem_t)));
+
+			//重置rear和front
+			q->rear += q->capacity - q->front; //等价于q->rear = q->capacity - 1
+			q->front = 0;
+		}
+		free(q->elems);	//释放原有的内存空间
+		q->elems = tmp;
+		q->capacity *= 2;
+	}
+	q->elems[q->rear] = x;
+	q->rear = (q->rear + 1) % q->capacity;	//因为是循环存储，所以注意取模
+}
+
+///@brief 获取队首元素
+///@param[in] q 队列对象的指针
+///@return 队首元素
+queue_elem_t queue_front(const queue_t* q)
+{
+	return q->elems[q->front];
+}
+
+///@brief 获取队尾元素
+///@param[in] q 队列对象的指针
+///@return 队尾元素
+queue_elem_t queue_rear(const queue_t* q)
+{
+	return q->elems[q->rear];
+}
+
+///@brief 弹出队首元素
+///@param[in] q 队列指针
+///@return 无
+void queue_pop(queue_t* q)
+{
+	q->front = (q->front + 1) % q->capacity;
+	return;
+}
 int main()
 {
 	return 0;
