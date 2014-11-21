213858deff25cc521f6dc5038c9b9327ef9d36b0
Zhao Wei <pkuzw@126.com>
2014-11-21 20:51:26 +0800

    实现Morris先序遍历二叉树函数
    Morris先序遍历算法的步骤如下：
    1. 初始化当前节点cur为root节点
    2. 如果cur没有左孩子，则输出当前节点，并将当前节点更新为其右孩子，即cur = cur->rchild
    3. 如果cur有左孩子，则寻找其前驱节点，即左子树的最右下角节点：
    a). 如果当前节点的前驱节点的右孩子为空，则输出当前节点，并将前驱节点的右孩子指向当前节点，然后将当前节点更新为其左孩子，即cur =
    cur->lchild
    b). 如果当前节点的前驱节点的右孩子指向当前节点，则断开该指向，将前驱节点的右孩子指向空指针，然后将当前节点更新为其右孩子，即cur =
    cur->rchild
    4. 重复步骤2、3，直至cur为空

--- Morris_Traversal.cpp
+++ Morris_Traversal.cpp
@@ -69,6 +69,52 @@ void in_order_morris(bt_node_t* root, int(*visit)(elem_t))
 	}
 }
 
+///@brief Morris先序遍历二叉树
+///@param[in] root 根节点
+///@param[in] visit 访问节点数据函数
+///@return 无
+/* @note Morris先序遍历算法的步骤如下：
+		 1. 初始化当前节点cur为root节点
+		 2. 如果cur没有左孩子，则输出当前节点，并将当前节点更新为其右孩子，即cur = cur->rchild
+		 3. 如果cur有左孩子，则寻找其前驱节点，即左子树的最右下角节点：
+			a). 如果当前节点的前驱节点的右孩子为空，则输出当前节点，并将前驱节点的右孩子指向当前节点，然后将当前节点更新为其左孩子，即
+			cur = cur->lchild
+			b). 如果当前节点的前驱节点的右孩子指向当前节点，则断开该指向，将前驱节点的右孩子指向空指针，然后将当前节点更新为其右孩子，即
+			cur = cur->rchild
+		 4. 重复步骤2、3，直至cur为空
+*/
+void pre_order_morris(bt_node_t* root, int (*visit)(elem_t))
+{
+	bt_node_t *cur = root, *pre = NULL;
+	while (cur != NULL)
+	{
+		if (cur->lchild == NULL)	//如果没有左孩子，则访问当前节点，并将当前节点更新为其右孩子
+		{
+			visit(cur->data);
+			cur = cur->rchild;
+		}
+		else						//如果有左孩子
+		{
+			pre = cur->lchild;		//寻找当前节点的前驱节点
+			while (pre->rchild != NULL && pre->rchild != cur)
+			{
+				pre = pre->rchild;
+			}
+			if (pre->rchild == NULL)//如果前驱未指向当前节点，则访问当前节点，将前驱节点右孩子指向当前节点，更新为当前节点的左孩子
+			{	
+				visit(cur->data);
+				pre->rchild = cur;
+				cur = cur->lchild;
+			}
+			else if (pre->rchild == cur)//如果前驱节点指向当前节点，则断开该指向，并将当前节点更新为当前节点的右孩子
+			{
+				pre->rchild == NULL;
+				cur = cur->rchild;
+			}
+		}
+	}
+}
+
 int main()
 {
 	return 0;
