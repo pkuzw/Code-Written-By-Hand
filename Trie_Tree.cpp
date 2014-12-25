445adb3c1e7473215a688bde2dc2f3005c762cf1
Zhao Wei <pkuzw@126.com>
2014-12-25 21:10:15 +0800

    写"Immediate Decodebility"问题注释

--- Trie_Tree.cpp
+++ Trie_Tree.cpp
@@ -8,6 +8,49 @@
    在插入时也是一样，从根节点向下找，找到不一样的节点时开始插入字符和值。
    删除时是将节点值置为0，如果连接到该节点的父节点值也为零，则将该节点删除，否则保留。
    参考文献：http://algs4.cs.princeton.edu/lectures/52Tries.pdf
+
+   5.8.2 Immediate Decodebility
+   描述
+   An encoding of a set of symbols is said to be immediately decodable if no code for one symbol is the
+   prefix of a code for another symbol. We will assume for this problem that all codes are in binary, that no
+   two codes within a set of codes are the same, that each code has at least one bit and no more than ten bits,
+   and that each set has at least two codes and no more than eight.
+   Examples: Assume an alphabet that has symbols {A, B, C, D}.
+   following code is immediately decodable:
+   A:01 B:10 C:0010 D:0000
+   but this one is not:
+   A:01 B:10 C:010 D:0000 (Note that A is a prefix of C)
+
+   输入
+   Write a program that accepts as input a series of groups of records from standard input. Each record
+   in a group contains a collection of zeroes and ones representing a binary code for a different symbol. Each
+   group is followed by a single separator record containing a single 9; the separator records are not part of
+   the group. Each group is independent of other groups; the codes in one group are not related to codes in
+   any other group (that is, each group is to be processed independently).
+
+   输出
+   For each group, your program should determine whether the codes in that group are immediately
+   decodable, and should print a single output line giving the group number and stating whether the group is,
+   or is not, immediately decodable.
+
+   样例输入
+   01
+   10
+   0010
+   0000
+   9
+   01
+   10
+   010
+   0000
+   9
+
+   样例输出
+   Set 1 is immediately decodable
+   Set 2 is not immediately decodable
+
+   分析
+   判断一个串是否是另一个串的前缀，这正是Trie 树（即字典树）的用武之地。
 */
 
 #include <iostream>
