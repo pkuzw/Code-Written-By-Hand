d073c6deff8faec7bb486218aaaa3a5b5098cdac
Zhao Wei <pkuzw@126.com>
2014-12-27 15:22:50 +0800

    写问题"Hardwood Species"的描述
    1. 该题来自于POJ 2418，输入数据量比较大，应该注意避免使用cin，以免超时。

---
+++ Hardwood_Species.cpp
@@ -0,0 +1,72 @@
+﻿///@file Hardwood Species
+///@author zhaowei
+///@date 2014.12.27
+///@version 1.0
+/* @note 
+   描述
+   现在通过卫星扫描，扫描了很多区域的树，并获知了每棵树的种类，求每个种类的百分比。
+
+   输入
+   一行一棵树，表示该树的种类。每个名字不超过30个字符，树的种类不超过10,000；树的总数不超过1,000,000.
+
+   输出
+   按字母顺序，打印每个种类的百分比，精确到小数点后4位
+
+   样例输入
+   Red Alder
+   Ash
+   Aspen
+   Basswood
+   Ash
+   Beech
+   Yellow Birch
+   Ash
+   Cherry
+   Cottonwood
+   Ash
+   Cypress
+   Red Elm
+   Gum
+   Hackberry
+   White Oak
+   Hickory
+   Pecan
+   Hard Maple
+   White Oak
+   Soft Maple
+   Red Oak
+   Red Oak
+   White Oak
+   Poplan
+   Sassafras
+   Sycamore
+   Black Walnut
+   Will
+
+   样例输出
+   Ash 13.7931
+   Aspen 3.4483
+   Basswood 3.4483
+   Beech 3.4483
+   Black Walnut 3.4483
+   Cherry 3.4483
+   Cottonwood 3.4483
+   Cypress 3.4483
+   Gum 3.4483
+   Hackberry 3.4483
+   Hard Maple 3.4483
+   Hickory 3.4483
+   Pecan 3.4483
+   Poplan 3.4483
+   Red Alder 3.4483
+   Red Elm 3.4483
+   Red Oak 6.8966
+   Sassafras 3.4483
+   Soft Maple 3.4483
+   Sycamore 3.4483
+   White Oak 10.3448
+   Willow 3.4483
+   Yellow Birch 3.4483
+
+   本题来自POJ 2418
+*/
