b87b128f45af9524a22fedd2c405a3e74777df2f
Zhao Wei <pkuzw@126.com>
2014-12-30 23:18:12 +0800

    新建一个源文件

---
+++ Hardwood_Species1.cpp
@@ -0,0 +1,9 @@
+//
+//  Hardwood_Species1.cpp
+//  Code Written By Hand
+//
+//  Created by Zhao Wei on 14/12/30.
+//  Copyright (c) 2014年 Zhao Wei. All rights reserved.
+//
+
+#include "Hardwood_Species1.h"
---
+++ Hardwood_Species1.h
@@ -0,0 +1,14 @@
+//
+//  Hardwood_Species1.h
+//  Code Written By Hand
+//
+//  Created by Zhao Wei on 14/12/30.
+//  Copyright (c) 2014年 Zhao Wei. All rights reserved.
+//
+
+#ifndef __Code_Written_By_Hand__Hardwood_Species1__
+#define __Code_Written_By_Hand__Hardwood_Species1__
+
+#include <stdio.h>
+
+#endif /* defined(__Code_Written_By_Hand__Hardwood_Species1__) */
