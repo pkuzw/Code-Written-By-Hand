f75640981fddb82fc630839637b3d1d101940aab
Zhao Wei <pkuzw@126.com>
2014-12-30 22:19:25 +0800

    完成哈希表的实现

--- Hash_Set.cpp
+++ Hash_Set.cpp
@@ -39,10 +39,10 @@ public:
 private:
 	int prime;		//哈希表取模的质数，即哈希桶的个数，小于capacity
 	int capacity;	//哈希表容量，一定要大于元素最大个数
-	int *head;		//首结点下标
+	int *head;		//哈希表槽中链表首结点下标
 
-	//静态链表
-	struct node_t
+	
+	struct node_t	//静态链表
 	{
 		elem_t elem;
 		int next;
@@ -51,3 +51,58 @@ private:
 
 	int size; //实际元素个数
 };
+
+template<typename elem_t>
+hash_set<elem_t>::hash_set(int prime, int capacity)
+{
+	this->prime = prime;
+	this->capacity = capacity;
+	head = new int[prime];
+	node = new node_t[capacity];
+	fill(head head + prime, -1);	//将head填满-1
+	fill(node, node + capacity, node_t());	//将node填满node_t()
+	size = 0;	
+}
+
+template<typename elem_t>
+hash_set<elem_t>::~hash_set()
+{
+	this->prime = 0;
+	this->capacity = 0;
+	delete[] head;
+	delete[] node;
+	head = NULL;
+	node = NULL;
+	size = 0;
+}
+
+template<typename elem_t>
+bool hash_set<elem_t>::find(const elem_t &elem)
+{
+	for (int i = head[elem_hash(elem)]; i != -1; i = node[i].next)
+		if (elem == node[i].elem)
+		{
+			return true;
+		}
+	return false;	
+}
+
+template<typename elem_t>
+bool hash_set<elem_t>::insert(const elem_t &elem)
+{
+	const int hash_code = elem_hash(elem);
+
+	for (int i = head[hash_code]; i != -1; i = node[i].next)
+	{
+		if (elem == node[i].elem)
+		{
+			return false;	//如果已经存在，返回false
+		}
+
+		//如果不存在，则插入首结点之前
+		node[size].next = head[hash_code];
+		node[size].elem = elem;
+		head[hash_code] = size++;	//??
+		return true;
+	}
+}
