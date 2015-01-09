830572eb42259cc0ab34555c648d3310b5a9a2ed
Zhao Wei <pkuzw@126.com>
2015-01-09 15:15:09 +0800

    写问题“打印1到n的所有排列”
    1. 主体没写，只写了主函数和注释等周边代码。

---
+++ Print_Permutation.cpp
@@ -0,0 +1,33 @@
+﻿///@file 打印1到n的所有排列
+///@author zhaowei
+///@date 2015.01.09
+///@version 1.0
+
+#include <iostream>
+using namespace std;
+
+///@brief 打印1到n的所有排列
+///@param[in] n 数组的长度
+///@return 无
+void print_permutation(const int n)
+{
+	int prmt_cnt = n;
+	for (int i = n-1; i > 0; i--)
+	{
+		prmt_cnt *= i;
+	}
+	int *permutation = new int[prmt_cnt];
+
+	delete[] permutation;
+}
+
+int main()
+{
+	cout << "Input n: ";
+	int n = 0;
+	cin >> n;
+
+	print_permutation(n);
+
+	return 0;
+}
