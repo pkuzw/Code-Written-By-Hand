d950c21419737fdf9be58cc0e91fc6fc6e71a94c
Zhao Wei <pkuzw@126.com>
2015-01-12 23:33:26 +0800

    写问题“打印1到n的所有排列”
    1. 递归求解，先输出以1开头的排列，再输出以2开头的，最后输出以n开头的。
    2. 这道题递归的思想还需要再理解一下，今天没弄懂。

--- Print_Permutation.cpp
+++ Print_Permutation.cpp
@@ -2,9 +2,42 @@
 ///@author zhaowei
 ///@date 2015.01.09
 ///@version 1.0
+///@note 递归求解，先输出以1开头的排列，然后输出以2开头的排列，最后输出以n开头的排列。
 
 #include <iostream>
 using namespace std;
+
+///@brief 递归打印1到n的全排列
+///@param[in] n 排列的最大元素数
+///@param[in] cur 当前递归的位置
+///@param[out] permutation 存放的排列
+///@return 无
+static void print_permutation_r(const int n, int cur, int* permutation)
+{
+	if (cur == n)
+	{
+		for (int i = 0; i < n; i++)
+			cout << permutation[i] << " ";
+		cout << endl;
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		int used = 0;
+		for (int j = 0; j < cur; j++)
+		{
+			if (permutation[j] == i)
+			{
+				used = 1;	//如果i已经在permutation[0...cur-1]出现过，则不能再选
+			}
+		}
+		if (!used)
+		{
+			permutation[cur] = i;
+			print_permutation_r(n, cur + 1, permutation);	//递归调用
+		}
+	}
+}
 
 ///@brief 打印1到n的所有排列
 ///@param[in] n 数组的长度
@@ -12,12 +45,8 @@ using namespace std;
 void print_permutation(const int n)
 {
 	int prmt_cnt = n;
-	for (int i = n-1; i > 0; i--)
-	{
-		prmt_cnt *= i;
-	}
 	int *permutation = new int[prmt_cnt];
-
+	print_permutation_r(n, 0, permutation);
 	delete[] permutation;
 }
 
