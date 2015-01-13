e0ca73fe9864d97604d8cd3d702ae84354ded5bf
Zhao Wei <pkuzw@126.com>
2015-01-13 23:42:08 +0800

    写“求集合子集”问题
    1. 写了部分主函数，主体思路和函数还没有写。

--- Subset.cpp
+++ Subset.cpp
@@ -1,4 +1,32 @@
-﻿///@file 给定一个集合，求出其所有子集
+﻿///@file 给定一个集合，求出其所有子集，假设集合中没有重复元素
 ///@author zhaowei
 ///@date 2015.01.10
 ///@version 1.0
+///@note 
+/* 增量构造法	一次选出一个元素，放或者不放入到集合中
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+	vector<string> svec;
+	cout << "Input elements of the set, end as \"EOF\": ";
+	string str;
+	int cnt = 0;
+	while (cin >> str && str != "EOF")
+	{
+		svec.push_back(str);
+		cnt++;
+	}
+
+	for (int i = 0; i < cnt; i++)
+	{
+		cout << svec[i] << " ";
+	}
+	cout << endl;
+	return 0;
+}
