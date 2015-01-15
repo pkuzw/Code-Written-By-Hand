f5c39deec7efa6d27277e0c92236ed127b8662db
Zhao Wei <pkuzw@126.com>
2015-01-15 21:12:58 +0800

    实现增量构造法求子集
    1. 递归的思路求解；
    2. 将原本的整数集合扩展成字符串集合。

--- Subset.cpp
+++ Subset.cpp
@@ -11,22 +11,48 @@
 #include <string>
 using namespace std;
 
+///@brief 增量构造法
+///@param[in] S 输入集合
+///@param[in] n 集合大小
+///@param[inout] P 某个子集
+///@param[in] cur P的当前位置
+///@param[in] ed S的当前位置，前面的元素已经选过了
+///@return 无
+void print_subset1(vector<string> S, int n, vector<string> P, int cur, int ed)
+{
+	for (int i = ed; i < n; i++)
+	{
+		P[cur] = S[i];
+		for (int j = 0; j <= cur; j++)
+		{
+			cout << P[j] << " ";
+		}
+		cout << endl;
+		print_subset1(S, n, P, cur + 1, i + 1);
+	}
+}
+
 int main()
 {
-	vector<string> svec;
-	cout << "Input elements of the set, end as \"EOF\": ";
+	vector<string> svec, pvec;
+	cout << "Input elements of the set, end as \"EOF\": " << endl;
 	string str;
 	int cnt = 0;
 	while (cin >> str && str != "EOF")
 	{
 		svec.push_back(str);
+		pvec.push_back("");
 		cnt++;
 	}
-
+	cout << "---------------------------------------------------------\n";
+	cout << "The entire set is: " << endl;
 	for (int i = 0; i < cnt; i++)
 	{
 		cout << svec[i] << " ";
 	}
 	cout << endl;
+	cout << "---------------------------------------------------------\n";
+	cout << "The subsets are: " << endl;
+	print_subset1(svec, cnt, pvec, 0, 0);
 	return 0;
 }
